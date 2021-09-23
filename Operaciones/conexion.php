6a8ee67448035ccbfe471ea869426d9493c65a27
Milisay-davila <82669770+Milisay-davila@users.noreply.github.com>
2021-09-23 03:25:44 -0500

    Create conexion.php

---
+++ Operaciones/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$host="localhost";
+$username="root";
+$bdname="registro";
+//Permite realizar conexion con la base de datos creada.
+$conexion= mysqli_connect('localhost','root','','registro');
+?>
+
+ 
