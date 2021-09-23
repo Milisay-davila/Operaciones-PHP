f2bb61ff8d69e879f44e9a8fca163b0499f80029
Milisay-davila <82669770+Milisay-davila@users.noreply.github.com>
2021-09-23 03:25:55 -0500

    Create index.php

---
+++ Operaciones/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<!--le indica al navegador que todo el contenido esta en español!-->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesion Compensar</title>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+    <center>
+<form action="validar.php" method="post">
+        <h1>Ingresar los siguientes datos</h1>
+        <p>Usuario:<input type="text" placeholder="Ingrese su nombre" name="Usuario"></p>
+        <p>Email:<input type="text" placeholder="Ingrese su contraseña" name="Email"></p>
+        <input type="submit" value="Ingresar">
+        <h2>La mejor tienda, que piensa en ti</h2> 
+</center>
+<!--Formulario para registrar los datos en la base de datos!-->
+    </form> 
+</body>
+</html>
